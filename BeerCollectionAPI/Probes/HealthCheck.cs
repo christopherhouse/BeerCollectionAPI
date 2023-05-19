71bad63b5dc2d4e734fc5f03b4633ecc175533a3
Chris House <chhouse@microsoft.com>
2023-05-19 14:49:56 -0500

    added healthcheck

---
+++ BeerCollectionAPI/Probes/HealthCheck.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace BeerCollectionAPI.Probes;
+
+public class HealthCheck : IHealthCheck
+{
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+    {
+        var isHealthy = true; // TODO:  Do some more sophisticated health checking
+
+        return isHealthy
+            ? Task.FromResult(HealthCheckResult.Healthy("Healthy"))
+            : Task.FromResult(new HealthCheckResult(context.Registration.FailureStatus, "Unhealthy"));
+    }
+}
